e29c555be058a24e403b34a5f57bb19f4e00d8e0
zama1024 <farshid_bd@hotmail.com>
2017-03-05 00:09:48 -0500

    Add obstacles

--- bundle.js
+++ bundle.js
@@ -85,21 +85,27 @@ document.addEventListener('DOMContentLoaded', ()=> {
 /***/ (function(module, exports, __webpack_require__) {
 
 const Player = __webpack_require__(3);
+const Obstacle = __webpack_require__(5);
 class Game {
   constructor(backgroundCtx,foregroundCtx){
     this.background = null;
     this.createBackground(backgroundCtx,foregroundCtx);
+    this.interval = setInterval(() => {this.velocity += 1;}, 100);
+    this.velocity = 300;
   }
 
   createBackground(backgroundCtx, foregroundCtx) {
-    var velocity=200;
+    var velocity=this.velocity;
     const backgroundImage = new Image();
     backgroundImage.src = './assets/images/citybackground.jpg';
     const player = new Player(foregroundCtx, 40, 415);
+
+    const taxi = new Obstacle(foregroundCtx, 400, 300);
+
     const drawImage = (time) => {
       var framegap=time-lastRepaintTime;
       lastRepaintTime=time;
-      var translateX=velocity*(framegap/900);
+      var translateX=this.velocity*(framegap/900);
       backgroundCtx.clearRect(0,0,1100,650);
       var pattern=backgroundCtx.createPattern(backgroundImage,"repeat-x");
       backgroundCtx.fillStyle=pattern;
@@ -120,8 +126,12 @@ module.exports = Game;
 /***/ }),
 /* 2 */,
 /* 3 */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
+const Taxi = __webpack_require__(6);
+const Barrier = __webpack_require__(7);
+const Flinstones = __webpack_require__(9);
+const Car = __webpack_require__(10);
 class Player {
   constructor(ctx, x, y) {
     this.speedX = 0;
@@ -133,13 +143,16 @@ class Player {
     this.timerCount = 1;
     this.jump = false;
     this.getInitHeight = 0;
-    this.gravity = 6;
+    this.gravity = 5;
     this.gravitySpeed = 0;
-    const bikerImage = new Image();
-
-    this.image = bikerImage;
-    this.interval = setInterval(this.updateGame.bind(this), 10);
     this.pressedKey = false;
+    this.obs1 = new Barrier(this.ctx);
+    this.frameNumber = 0;
+
+    const bikerImage = new Image();
+    this.image = bikerImage;
+
+    this.interval = setInterval(this.updatePlayer.bind(this), 10);
     window.addEventListener('keydown', function (e) {
       Player.pressedKey = (Player.pressedKey || []);
       Player.pressedKey[e.keyCode] = true;
@@ -149,15 +162,22 @@ class Player {
     });
   }
 
-  update(){
+  update(taxi){
     let photoCountChar;
     if (this.photoCount > 2) {
       this.photoCount = 1;
     }
-    photoCountChar = this.photoCount > 9 ? `${this.photoCount}` : `0${this.photoCount}`;
-    this.image.src = `./assets/images/image_part_0${photoCountChar}.png`;
+
+    this.image.src = `./assets/images/image_part_00${this.photoCount}.png`;
     this.ctx.clearRect(0,0,1100,650);
-    this.ctx.drawImage(this.image, this.x, this.y);
+    if(this.obs1.y > this.y){
+
+      this.ctx.drawImage(this.image, this.x, this.y,230,230);
+      this.ctx.drawImage(this.obs1.image, this.obs1.x, this.obs1.y ,this.obs1.width,this.obs1.height);
+    }else{
+      this.ctx.drawImage(this.obs1.image, this.obs1.x, this.obs1.y ,this.obs1.width,this.obs1.height);
+      this.ctx.drawImage(this.image, this.x, this.y,230,230);
+    }
   }
 
   newPos() {
@@ -165,39 +185,43 @@ class Player {
       this.gravitySpeed = this.gravity;
       this.y += this.speedY + this.gravitySpeed;
       this.x += this.speedX;
-    }
-    if (this.getInitHeight === this.y) {
-      this.jump = false;
-    }
+    }else {
       this.y += this.speedY;
       this.x += this.speedX;
-    if (this.y > 350) {
-      this.y = 350;
     }
-    if (this.x > 900) {
-      this.x = 900;
+
+    if (this.getInitHeight <= this.y) {
+      this.jump = false;
+      this.getInitHeight = 0;
+    }
+
+    if (this.y > 400) {
+      this.y = 400;
+    }
+
+    if (this.x > 700) {
+      this.x = 700;
     }
     this.speedX = 0;
     this.speedY = 0;
   }
 
-  updateGame(){
+  updatePlayer(){
+    this.frameNumber += 1;
     this.newPos();
-    if (Player.pressedKey && Player.pressedKey[37] && this.x > 100) {this.speedX = -2; }
-    if (Player.pressedKey && Player.pressedKey[39] && this.x < 950) {this.speedX = 2; }
-    if (Player.pressedKey && Player.pressedKey[38] && this.y > 220) {this.speedY = -2; }
-    if (Player.pressedKey && Player.pressedKey[40] && this.y < 350) {this.speedY = 2; }
+    if (Player.pressedKey && Player.pressedKey[37] && this.x > 50) {this.speedX = -2; }
+    if (Player.pressedKey && Player.pressedKey[39] && this.x < 700) {this.speedX = 2; }
+    if (Player.pressedKey && Player.pressedKey[38] && this.y > 270) {this.speedY = -2; }
+    if (Player.pressedKey && Player.pressedKey[40] && this.y < 400) {this.speedY = 2; }
     if (Player.pressedKey && Player.pressedKey[32]) {
       Player.pressedKey = false;
       this.jump = true;
       this.getInitHeight = this.y;
-      this.speedY = -100;
-      this.speedX = 0;
+      this.speedY = -200;
+      this.speedX = 50;
     }
-    // if (this.x < 600) {
-    // }
-    // if (this.y > 300) {
-    // }
+
+    this.obs1.x -= 6;
     this.timerCount += 1;
     if (this.timerCount === 20) {
       this.photoCount += 1;
@@ -206,25 +230,122 @@ class Player {
     this.update();
   }
 
-
-
-  moveup() {
-    this.y -= 1;
+  everyinterval(n) {
+    if ((this.frameNumber / n) % 1 === 0) {
+      return true;
+    }
+    return false;
   }
 
-  movedown() {
-    this.y += 1;
-  }
-
-  moveleft() {
-    this.x -= 1;
-  }
-
-  moveright() {
-    this.x += 1;
-  }
 }
 module.exports = Player;
+
+
+/***/ }),
+/* 4 */,
+/* 5 */
+/***/ (function(module, exports) {
+
+class Taxi{
+  constructor(ctx, x, y) {
+    this.speedX = 0;
+    this.speedY = 0;
+    this.x = x;
+    this.y = y;
+    this.ctx = ctx;
+    const taxiImage = new Image();
+    taxiImage.src = './assets/images/taxi.png';
+    this.image = taxiImage;
+  }
+
+
+}
+module.exports = Taxi;
+
+
+/***/ }),
+/* 6 */
+/***/ (function(module, exports) {
+
+class Taxi{
+  constructor(ctx, x, y) {
+    this.height = 250;
+    this.width = 400;
+    this.x = 1100;
+    this.y = [270,400][Math.floor(Math.random()*2)];
+    this.ctx = ctx;
+    const taxiImage = new Image();
+    taxiImage.src = './assets/images/taxi.png';
+    this.image = taxiImage;
+  }
+
+
+}
+module.exports = Taxi;
+
+
+/***/ }),
+/* 7 */
+/***/ (function(module, exports) {
+
+class Barrier{
+  constructor(ctx, x, y) {
+    this.height = 100;
+    this.width = 75;
+    this.x = 1100;
+    this.y = [425,480,540][Math.floor(Math.random()*3)];
+    this.ctx = ctx;
+    const barrierImage = new Image();
+    barrierImage.src = './assets/images/barrier.png';
+    this.image = barrierImage;
+  }
+
+
+}
+module.exports = Barrier;
+
+
+/***/ }),
+/* 8 */,
+/* 9 */
+/***/ (function(module, exports) {
+
+class Flinstones{
+  constructor(ctx) {
+    this.height = 200;
+    this.width = 350;
+    this.x = 1100;
+    this.y = [300,430][Math.floor(Math.random()*2)];
+    this.ctx = ctx;
+    const flinstonesImage = new Image();
+    flinstonesImage.src = './assets/images/flinstones.png';
+    this.image = flinstonesImage;
+  }
+
+
+}
+module.exports = Flinstones;
+
+
+/***/ }),
+/* 10 */
+/***/ (function(module, exports) {
+
+class Car{
+  constructor(ctx) {
+    this.height = 220;
+    this.width = 280;
+    this.x = 1100;
+    this.y = [300,400][Math.floor(Math.random()*2)];
+    this.ctx = ctx;
+    const carImage = new Image();
+    carImage.src = './assets/images/car.png';
+    this.image = carImage;
+  }
+
+
+}
+module.exports = Car;
 
 
 /***/ })
