909ba9e5f5aa732959d795200c17077c1c765d50
zama1024 <farshid_bd@hotmail.com>
2017-03-08 12:07:23 -0500

    Update background and game logic

--- bundle.js
+++ bundle.js
@@ -81,22 +81,22 @@ class Game {
   createBackground(backgroundCtx, foregroundCtx) {
     var velocity=this.velocity;
     const backgroundImage = new Image();
-    backgroundImage.src = './assets/images/citybackground.jpg';
+    backgroundImage.src = './assets/images/rsz_citybackground.jpg';
 
     const drawImage = (time) => {
       var framegap=time-lastRepaintTime;
       lastRepaintTime=time;
       var translateX=(this.velocity += .2)*(framegap/900);
-      backgroundCtx.clearRect(0,0,1100,650);
+      backgroundCtx.clearRect(0,0,850,475);
       var pattern=backgroundCtx.createPattern(backgroundImage,"repeat-x");
       backgroundCtx.fillStyle=pattern;
-      backgroundCtx.rect(translateX,0,1100,650);
+      backgroundCtx.rect(translateX,0,850,475);
       backgroundCtx.fill();
       backgroundCtx.translate(-translateX,0);
       window.requestId = requestAnimationFrame(drawImage);
     };
     backgroundImage.addEventListener('load',drawImage,false);
-    const player = new Player(backgroundCtx, foregroundCtx, 40, 415);
+    const player = new Player(backgroundCtx, foregroundCtx, 40, 250);
     var lastRepaintTime=window.performance.now();
   }
 
@@ -111,17 +111,18 @@ module.exports = Game;
 
 class Barrier{
   constructor(ctx, x, y) {
-    this.height = 150;
-    this.width = 75;
+    this.height = 75;
+    this.width = 40;
     this.x = 1100;
-    this.y = [425,480,510][Math.floor(Math.random()*3)];
+    this.y = [280,320,360][Math.floor(Math.random()*3)];
     this.ctx = ctx;
-    this.lane1 = this.y > 330;
-    this.lane2 = this.y < 315;
-    this.middlelane =  this.y === 480;
+    this.lane1 = this.y === 360;
+    this.lane2 = this.y === 280;
+    this.middlelane =  this.y === 320;
     const barrierImage = new Image();
     barrierImage.src = './assets/images/barrier.png';
     this.image = barrierImage;
+    this.type = "barrier";
   }
 
 
@@ -135,13 +136,13 @@ module.exports = Barrier;
 
 class Car{
   constructor(ctx) {
-    this.height = 220;
-    this.width = 280;
-    this.x = 1100;
-    this.y = [300,400][Math.floor(Math.random()*2)];
+    this.height = 160;
+    this.width = 210;
+    this.x = 1000;
+    this.y = [190,275][Math.floor(Math.random()*2)];
     this.ctx = ctx;
-    this.lane1 = this.y > 330;
-    this.lane2 = this.y < 315;
+    this.lane1 = this.y === 275;
+    this.lane2 = this.y === 190;
     const carImage = new Image();
     carImage.src = './assets/images/car.png';
     this.image = carImage;
@@ -158,13 +159,13 @@ module.exports = Car;
 
 class Flinstones{
   constructor(ctx) {
-    this.height = 200;
-    this.width = 350;
+    this.height = 175;
+    this.width = 320;
     this.x = 1100;
-    this.y = [300,430,365][Math.floor(Math.random()*3)];
-    this.lane1 = this.y === 430;
-    this.lane2 = this.y === 300;
-    this.middlelane =  this.y === 365;
+    this.y = [180,270,220][Math.floor(Math.random()*3)];
+    this.lane1 = this.y === 270;
+    this.lane2 = this.y === 180;
+    this.middlelane =  this.y === 220;
     this.ctx = ctx;
     const flinstonesImage = new Image();
     flinstonesImage.src = './assets/images/flinstones.png';
@@ -201,6 +202,7 @@ class Player {
     this.pressedKey = false;
     this.frameNumber = 0;
     this.obs1 = null;
+    this.score = 0;
     this.createObstacles = {
       0: () => new Barrier(this.ctx),
       1: () => new Taxi(this.ctx),
@@ -226,29 +228,36 @@ class Player {
     if (this.photoCount > 2) {
       this.photoCount = 1;
     }
-    if (this.frameNumber === 1 || this.everyinterval(125)) {
+    if (this.frameNumber === 1 || this.everyinterval(150)) {
       let randomIndex = (Math.floor(Math.random() * 4)).toString();
       this.obs1 = this.createObstacles[randomIndex]();
       this.checkCollision = true;
     }
     this.image.src = `./assets/images/image_part_00${this.photoCount}.png`;
-    this.ctx.clearRect(0,0,1100,650);
+    this.ctx.clearRect(0,0,850,475);
     if(this.obs1.y > this.y){
-
-      this.ctx.drawImage(this.image, this.x, this.y,230,230);
+      this.ctx.drawImage(this.image, this.x, this.y,175,175);
       this.ctx.drawImage(this.obs1.image, this.obs1.x, this.obs1.y ,this.obs1.width,this.obs1.height);
+      this.ctx.font = "30px Arial";
+      this.ctx.fillStyle = "#FF9900";
+      this.ctx.fillText(`Current Score: ${this.score}`,600,50);
     }else{
       this.ctx.drawImage(this.obs1.image, this.obs1.x, this.obs1.y ,this.obs1.width,this.obs1.height);
-      this.ctx.drawImage(this.image, this.x, this.y,230,230);
+      this.ctx.drawImage(this.image, this.x, this.y,175,175);
+      this.ctx.font = "30px Arial";
+      this.ctx.fillStyle = "#FF9900";
+      this.ctx.fillText(`Current Score: ${this.score}`,600,50);
     }
     if (this.checkCollision && this.crashWith(this.obs1)) {
-      debugger
+      this.ctx.font = "60px Arial";
+      this.ctx.fillStyle = '#000000';
+      this.ctx.fillRect(0,100,850,100);
+      this.ctx.fillStyle = "#FF9900";
+      this.ctx.fillText("Oops! Game over, Try again!",30,150);
       clearInterval(this.interval);
       const gameOver = document.getElementById('gameover');
       const gameOverCtx = gameOver.getContext('2d');
-      this.backgroundCtx.fillStyle="black";
-      this.backgroundCtx.fillRect(0,0,1100,650);
-      this.backgroundCtx.globalAlpha=0.2;
+
     }
   }
 
@@ -267,18 +276,24 @@ class Player {
       this.getInitHeight = 0;
     }
 
-    if (this.y > 400) {
-      this.y = 400;
+    if (this.y > 260) {
+      this.y = 260;
     }
-    if (this.y > 330){
+
+    if(this.y < 170){
+      this.lane1 = false;
+      this.lane2 = false;
+      this.middlelane = false;
+    }
+    if (this.y >= 212){
       this.lane1 = true;
       this.lane2 = false;
       this.middlelane = false;
-    } else if(this.y < 315){
+    } else if(this.y <= 201 && this.y > 170){
       this.lane1 = false;
       this.lane2 = true;
       this.middlelane = false;
-    } else if (this.y > 315 && this.y < 330){
+    } else if (this.y > 201 && this.y < 212){
       this.lane1 = false;
       this.lane2 = false;
       this.middlelane = true;
@@ -293,35 +308,35 @@ class Player {
 
   updatePlayer(){
     this.frameNumber += 1;
-    this.newPos();
     if (Player.pressedKey && Player.pressedKey[37] && this.x > 50) {this.speedX = -2; }
     if (Player.pressedKey && Player.pressedKey[39] && this.x < 700) {this.speedX = 2; }
-    if (Player.pressedKey && Player.pressedKey[38] && this.y > 270) {this.speedY = -2; }
+    if (Player.pressedKey && Player.pressedKey[38] && this.y > 170) {this.speedY = -2; }
     if (Player.pressedKey && Player.pressedKey[40] && this.y < 400) {this.speedY = 2; }
-    if (Player.pressedKey && Player.pressedKey[32] && !this.jump) {
+    if (Player.pressedKey && Player.pressedKey[32] && !this.jump && this.obs1.type === "barrier") {
       Player.pressedKey = false;
       this.jump = true;
       this.getInitHeight = this.y;
-      this.speedY = -200;
-      this.speedX = 50;
+      this.speedY = -150;
+      this.speedX = 70;
     }
     if (this.obs1){
 
-      this.obs1.x -= 10;
+      this.obs1.x -= 10.5;
     }
     this.timerCount += 1;
     if (this.timerCount === 20) {
       this.photoCount += 1;
       this.timerCount = 1;
     }
+    this.newPos();
     this.update();
   }
 
   crashWith(otherobj) {
     var playerLeft = this.x;
-    var playerRight = this.x + 180;
+    var playerRight = this.x + 140;
     var playerTop = this.y;
-    var playerBottom = this.y + 230;
+    var playerBottom = this.y + 175;
     var obstacleLeft = otherobj.x;
     var obstacleRight = otherobj.x;
     var obstacleTop = otherobj.y;
@@ -329,10 +344,13 @@ class Player {
     var crash = false;
     if (( (this.lane1 && otherobj.lane1) || (this.lane2 && otherobj.lane2)) && playerRight > obstacleLeft) {
       crash = true;
-    }else if ((this.middlelane && otherobj.middlelane) && playerRight > obstacleLeft) {
+    }else if ((otherobj.middlelane) && (playerRight > obstacleLeft) && (playerBottom < obstacleBottom) && (playerBottom > obstacleTop) && (playerTop > obstacleTop)) {
       crash = true;
-    }else if(playerRight > obstacleLeft) {
+    }else if ((otherobj.middlelane) && (this.middlelane) && (playerRight > obstacleLeft)) {
+      crash = true;
+    }else if(playerLeft > obstacleLeft) {
       this.checkCollision = false;
+      this.score += 1;
     }
 
     return crash;
@@ -355,13 +373,13 @@ module.exports = Player;
 
 class Taxi{
   constructor(ctx, x, y) {
-    this.height = 250;
-    this.width = 400;
-    this.x = 1100;
-    this.y = [270,400][Math.floor(Math.random()*2)];
+    this.height = 200;
+    this.width = 300;
+    this.x = 1000;
+    this.y = [170,250][Math.floor(Math.random()*2)];
     this.ctx = ctx;
-    this.lane1 = this.y > 330;
-    this.lane2 = this.y < 315;
+    this.lane1 = this.y === 250;
+    this.lane2 = this.y === 170;
     const taxiImage = new Image();
     taxiImage.src = './assets/images/taxi.png';
     this.image = taxiImage;
@@ -382,7 +400,41 @@ document.addEventListener('DOMContentLoaded', ()=> {
   const foregroundCanvas = document.getElementById('foreground');
   const backgroundCtx = backgroundCanvas.getContext('2d');
   const foregroundCtx = foregroundCanvas.getContext('2d');
-  const game = new Game(backgroundCtx, foregroundCtx);
+  const backgroundImage = new Image();
+  window.game = false;
+  backgroundImage.src = './assets/images/entrybackground.jpg';
+  backgroundImage.onload = function(){
+    backgroundCtx.drawImage(backgroundImage, 0, 0,850,475);
+  };
+  const logo = new Image();
+  logo.src = './assets/images/rclogo.png';
+  const keys = new Image();
+  keys.src = './assets/images/dirkeys.png';
+  const space = new Image();
+  space.src = './assets/images/space.png';
+  logo.onload = function(){
+    backgroundCtx.drawImage(logo, 110, 0,600,300);
+    backgroundCtx.font = "30px Verdana";
+    backgroundCtx.fillStyle = "#ffffff";
+    backgroundCtx.fillText("Press Enter to start game",220,280);
+    backgroundCtx.font = "15px Verdana";
+    backgroundCtx.fillStyle = "#FF9900";
+    backgroundCtx.fillText("Move around lanes",427,450);
+    backgroundCtx.fillText("Jump", 665,450);
+    backgroundCtx.fillStyle = "#7fffd4";
+// Turn transparency on
+    backgroundCtx.fillText("Dodge traffic by moving around lanes.", 80,350);
+    backgroundCtx.fillText("Use directional keys to change lane and", 80, 380);
+    backgroundCtx.fillText("press space to jump over wood logs.",80,410);
+     backgroundCtx.fillText("Jump is allowed for wood logs only.",80,440);
+    backgroundCtx.drawImage(keys, 430,350, 130, 80);
+    backgroundCtx.drawImage(space, 620,370, 130, 90);
+  };
+  window.addEventListener('keydown', function (e) {
+    if(e.keyCode === 13 && !window.game){
+      window.game = new Game(backgroundCtx, foregroundCtx);
+    }
+  },false);
 });
 
 
